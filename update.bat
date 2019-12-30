3bd836d3daebc2a241c36c947c398379c1de8cd6
Christopher Johnson <cmj.317@gmail.com>
2019-12-30 09:05:30 -0500

    20191230 update

--- update.bat
+++ update.bat
@@ -2,12 +2,12 @@ rem String input as only argument
 rem Commit -m 'date update'
 rem Push to remote
 
-set mdate=%date:~10,4%%date:~7,2%%date:~4,2%
+set mdate=%date:~10,4%%date:~4,2%%date:~7,2%
+
+cd %~dp0
 
 git pull
 update.py %1
 git add .
 git commit -m "%mdate% update"
 git push origin master
-
-echo %1
